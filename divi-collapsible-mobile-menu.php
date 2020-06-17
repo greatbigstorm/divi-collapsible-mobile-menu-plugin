7a427c2aa8ef819aec72eda2a82ed4dbd66ab15c
Sean Welder <sean@seanwelder.com>
2020-06-17 10:59:25 -0500

    Version bump

--- divi-collapsible-mobile-menu.php
+++ divi-collapsible-mobile-menu.php
@@ -6,10 +6,10 @@
  * Author:     Big Storm
  * Author URI: https://greatbigstorm.com/
  *
- * Version: 1.0.2
+ * Version: 1.1
  */
 
-define( 'DCMM_PLUGIN_VERSION', '1.0.2' );
+define( 'DCMM_PLUGIN_VERSION', '1.1' );
 
 define( 'DCMM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 add_action( 'init', function() {  // plugins path is filterable, so set after filters can be registered
