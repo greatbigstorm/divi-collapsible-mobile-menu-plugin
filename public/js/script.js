5660b0abaf8972e32159f49754620473d4a9b347
Sean Welder <sean@greatbigstorm.com>
2021-08-20 11:40:12 -0500

    Move window load listener outside DOM ready Event not always firing in Safari. Also replace deprecated `.load()` with `.on()`.

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,22 @@
-( function( $ ) {  // !# script loaded
+( function( $ ) {
 	// Sets up the mobile menu to be collapsible
 	function setup_collapsible_submenus() {
-		$('#mobile_menu')
+		$( '#mobile_menu' )
 			.addClass( 'dcmm-collapsible' )
 			.find( '.menu-item-has-children > a' )
 				.append( '<span class="dcmm-menu-toggle"></span>' )
 				.on( 'click', '.dcmm-menu-toggle', function( event ) {
 					event.preventDefault();
 					event.stopPropagation();
-					
+
 					$( this ).closest( '.menu-item' ).toggleClass( 'dcmm-visible' );
-				} )
+				} );
 	}
-	
-	$( function() {  // !# DOM ready
-		// Wait until after the Divi mobile menu has been initialized
-		$( window ).load( function() {
-			setTimeout( function() {
-				setup_collapsible_submenus();
-			}, 700 );
-		} );
+
+	// Wait until after the Divi mobile menu has been initialized
+	$( window ).on( 'load', function() {
+		setTimeout( function() {
+			setup_collapsible_submenus();
+		}, 700 );
 	} );
 } )( jQuery );
