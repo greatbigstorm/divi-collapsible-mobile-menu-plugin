cd7dba212dd17a1caf800bfb3c9e6815893aed08
Sean Welder <sean@seanwelder.com>
2021-08-19 23:27:01 -0500

    Set jQuery as a script dependency

--- divi-collapsible-mobile-menu.php
+++ divi-collapsible-mobile-menu.php
@@ -6,10 +6,10 @@
  * Author:     Big Storm
  * Author URI: https://greatbigstorm.com/
  *
- * Version: 1.2.1
+ * Version: 1.3.0
  */
 
-define( 'DCMM_PLUGIN_VERSION', '1.2.1' );
+define( 'DCMM_PLUGIN_VERSION', '1.3.0' );
 
 define( 'DCMM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 add_action( 'init', function() {  // plugins path is filterable, so set after filters can be registered
@@ -26,7 +26,7 @@ add_action( 'wp_enqueue_scripts', function () {
 
 
 	/* Scripts */
-	wp_enqueue_script( 'dcmm', DCMM_PLUGIN_URL . 'public/js/script.js', array(), DCMM_PLUGIN_VERSION, true );
+	wp_enqueue_script( 'dcmm', DCMM_PLUGIN_URL . 'public/js/script.js', array( 'jquery' ), DCMM_PLUGIN_VERSION, true );
 } );
 
 
