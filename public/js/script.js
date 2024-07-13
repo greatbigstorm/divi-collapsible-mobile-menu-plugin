a97dba1bfe02f4ea654c776c0d56afdc79941cae
Sean Welder <sean@greatbigstorm.com>
2024-07-12 17:11:19 -0700

    Make linkless parents full size toggle targets

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,32 @@
 ( function( $ ) {
+	var CLASS_TOGGLE_ITEM    = 'dcmm-menu-toggle',
+		CLASS_MENU_ENABLED   = 'dcmm-collapsible',
+		CLASS_SUBMENU_OPENED = 'dcmm-visible';
+
+
 	// Sets up the mobile menu to be collapsible
 	function setup_collapsible_submenus() {
 		$( '#mobile_menu' )
-			.addClass( 'dcmm-collapsible' )
+			.addClass( CLASS_MENU_ENABLED )
 			.find( '.menu-item-has-children > a' )
-				.append( '<span class="dcmm-menu-toggle"></span>' )
-				.on( 'click', '.dcmm-menu-toggle', function( event ) {
+				// For parent items with no link, make the entire link a toggle click target
+				// NOTE:  Do this first so `off()` does not remove the toggle handler below
+				.filter( '[href="#"]' )
+					.off( 'click' )  // removing Divi's handler that closes the menu
+					.on ( 'click', function( event ) {
+						event.preventDefault();
+						event.stopPropagation();
+
+						$( this ).children( '.' + CLASS_TOGGLE_ITEM ).trigger( 'click' );
+					} )
+					.end()
+				// ...now add the toggle element and handler
+				.append( '<span class="' + CLASS_TOGGLE_ITEM + '"></span>' )
+				.on( 'click', '.' + CLASS_TOGGLE_ITEM, function( event ) {
 					event.preventDefault();
 					event.stopPropagation();
 
-					$( this ).closest( '.menu-item' ).toggleClass( 'dcmm-visible' );
+					$( this ).closest( '.menu-item' ).toggleClass( CLASS_SUBMENU_OPENED );
 				} );
 	}
 
